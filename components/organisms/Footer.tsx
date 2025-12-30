01b1c44dcd85e010c13585d334aae333c4fea98e
suneelfm <sfm.20031998@gmail.com>
2025-12-30 16:19:03 +0530

    Created Footer

---
+++ components/organisms/Footer.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <Grid
+      container
+      height={"30px"}
+      className="items-center border-t border-t-[#22242d] whitespace-nowrap"
+    >
+      Footer concentrated on page design, so not developed the footer
+    </Grid>
+  );
+}
